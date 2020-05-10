93693221f0b1ac15a2f7b5e5cabce6b05f293261
brian30220 <brian30220@gmail.com>
2020-05-10 16:25:42 +0800

    第一次上傳

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/brian30220/pen/ZEbXvoZ](https://codepen.io/brian30220/pen/ZEbXvoZ).
+
+
---
+++ script.js
@@ -0,0 +1,15 @@
+// navbar
+// let menuBtn = document.getElementById("menu-icon");
+// let mobileHeader = document.getElementById("mobile-header");
+
+// menuBtn.onclick = function() {
+//   if (mobileHeader.classList.contains('open')) {
+//     menuBtn.classList.remove('close-icon');
+//     mobileHeader.classList.remove('open');
+//   } else {
+//     menuBtn.classList.add('close-icon');
+//     mobileHeader.classList.add('open');
+//   }
+// }
+
+// navbar
